798e6d9d46c32cd21375b0ef360b77d558925a1a
김여진 <ggthub777@gmail.com>
2024-09-03 12:11:14 +0900

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,37 @@
+package src;
+
+public class Test1 {
+public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("I'm a new Java pragrammer");
+    System.out.println("자바의 특징 세가지");
+    System.out.println("1.객체 지향");
+    System.out.println("2.구조독립적");
+    System.out.println("3.멀티스레드 지원");
+
+    int value = 100;
+    System.out.println("value : "+value);
+
+    value =0b1101000;
+    System.out.println("value :"+value);
+
+    value=0144;
+    System.out.println("value :"+value);
+
+    value = 0x64;
+    System.out.println("value : "+value);
+     /**literal->dinary, octal, hexadecimal etc... */
+}
+}
+/**자바 특징
+ * 1. 구조 독립
+ * 2.멀티스레드 지원 
+ * 3.객체지향 
+ * 
+ * 기초 자료형에 대응되는 wrapper class: 
+ * byte: byte short long int
+ * float:float double     
+ * boolean:boolean
+ * char:char
+ */
+
